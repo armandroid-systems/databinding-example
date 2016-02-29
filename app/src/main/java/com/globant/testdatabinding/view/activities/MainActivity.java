44a024f07a51f80513fb899c84f19ac7b64c46a9
Armando Zamorano <zad93wlf@gmail.com>
2016-02-29 00:08:21 -0600

    litle change

--- app/src/main/java/com/globant/testdatabinding/view/activities/MainActivity.java
+++ app/src/main/java/com/globant/testdatabinding/view/activities/MainActivity.java
@@ -2,14 +2,34 @@ package com.globant.testdatabinding.view.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.globant.testdatabinding.R;
+import com.globant.testdatabinding.utils.Constants;
+import com.globant.testdatabinding.utils.ScreenManager;
+import com.globant.testdatabinding.view.fragments.FragmentMain;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG =MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(findViewById(R.id.screenWrapper)!= null){
+            if(savedInstanceState == null){
+                try {
+                    ScreenManager.screenChange(MainActivity.this,
+                            R.id.screenWrapper,
+                            FragmentMain.class,null,
+                            Constants.VIEW_SUMMARY,
+                            Constants.BIN_TRUE);
+                } catch (IllegalAccessException e) {
+                    Log.e(TAG,"ERROR "+e);
+                } catch (InstantiationException e) {
+                    Log.e(TAG, "ERROR " + e);
+                }
+            }
+        }
     }
 }
