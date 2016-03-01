7fd608c3046c6ee68433ad3d94f3016191eb9769
armando.dominguez <armando.dominguez@globant.com>
2016-03-01 16:37:07 -0600

    added custom binder adapters

--- app/src/main/java/com/globant/testdatabinding/viewmodel/FormViewModel.java
+++ app/src/main/java/com/globant/testdatabinding/viewmodel/FormViewModel.java
@@ -1,15 +1,19 @@
 package com.globant.testdatabinding.viewmodel;
 
-import android.app.Service;
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import com.globant.testdatabinding.BR;
+import com.globant.testdatabinding.R;
 import com.globant.testdatabinding.adapter.RecyclerAdapter;
 import com.globant.testdatabinding.model.User;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by zadtankus on 28/02/16.
@@ -18,12 +22,20 @@ public class FormViewModel extends BaseObservable {
     private static final String TAG = FormViewModel.class.getSimpleName();
 
     private RecyclerAdapter mAdapter;
-    private String name;
-    private String phone;
-    private String email;
+    public   String name = "";
+    public  String phone = "";
+    public  String email = "";
 
     public FormViewModel(RecyclerAdapter mAdapter) {
         this.mAdapter = mAdapter;
+    }
+
+    @Bindable
+    public String getError(){
+        if(name.isEmpty() || phone.isEmpty() || email.isEmpty()){
+            return "THE FIELD CANNOT BE EMPTY";
+        }
+        return null;
     }
 
     public View.OnClickListener onClickButton(){
@@ -33,8 +45,9 @@ public class FormViewModel extends BaseObservable {
 
                 Log.d(TAG,"CLICK");
                 if(mAdapter.getArrayUser() != null){
-                    Log.d(TAG,"CLICK 2");
+                    Log.d(TAG,"CLICK 2"+name);
                     mAdapter.addItem(setUser());
+                    clearFields();
                     notifyChange();
                 }
 
@@ -46,22 +59,74 @@ public class FormViewModel extends BaseObservable {
         return mAdapter;
     }
 
-    public String getName() {
-        Log.d(TAG,"GETIN NAME");
-        return name;
-    }
-
-    public String getPhone() {
-        Log.d(TAG,"GETIN NAME 2");
-        return phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     public User setUser(){
-        return new User(getName(),getPhone(),getEmail());
+        return new User(name,phone, email);
     }
 
+    public TextWatcher getFieldWatcher(){
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                name = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        };
+    }
+
+    public EditText.OnFocusChangeListener getFocusListener(){
+        return new EditText.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    notifyPropertyChanged(BR.error);
+                }
+            }
+        };
+    }
+
+    @BindingAdapter("app:binding")
+    public static void alternative(final EditText v, final FormViewModel a){
+        v.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                switch(v.getId()){
+                    case R.id.editTextName:
+                        a.name = s.toString();
+                        break;
+                    case R.id.editTextPhone:
+                        a.phone = s.toString();
+                        break;
+                    case R.id.editTextMail:
+                        a.email = s.toString();
+                        break;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void clearFields(){
+        name = "";
+        phone = "";
+        email = "";
+    }
 }
